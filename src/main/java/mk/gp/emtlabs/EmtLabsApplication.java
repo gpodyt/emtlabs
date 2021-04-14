2124c47ff4e9d383f270707b580bbef66fc3091a
Марковиќ Марко <marko.markovikj@students.finki.ukim.mk>
2021-04-14 17:44:28 +0200

    Initial commit

---
+++ src/main/java/mk/gp/emtlabs/EmtLabsApplication.java
@@ -0,0 +1,13 @@
+package mk.gp.emtlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmtLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmtLabsApplication.class, args);
+    }
+
+}
